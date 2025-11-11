f19df338e49d3109c8bfb3078f6818057b797eff
AhmaddAbbass <ahmadmicrosoft20@gmail.com>
2025-11-11 23:39:35 +0200

    LAB10 initial commit hehe

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+# tests/test_app.py
+import unittest
+from app import greet
+
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(
+            greet("World"),
+            "Hello, World from FirstName LastName!"  # ← match your name & spacing above
+        )
+
+if __name__ == "__main__":
+    unittest.main()
