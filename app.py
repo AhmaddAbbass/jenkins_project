0980ea97cfa5094202d2acad9772f438312160ad
AhmaddAbbass <ahmadmicrosoft20@gmail.com>
2025-11-11 23:57:10 +0200

    fixed file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 STUDENT = "Ahmad Abbas" 
 
 def greet(name):
-    return f"Hello, {name} from {STUDENT}!"
+    return f"Hello, {name} from{STUDENT}!"
 
 if __name__ == "__main__":
     print(greet("World"))
